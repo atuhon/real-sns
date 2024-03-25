fa6da92807f00da095646ed3ff35717dcb8171f3
atuhon <kinnjiroa@gmail.com>
2024-03-25 21:34:06 +0900

    7

--- routes/post.js
+++ routes/post.js
@@ -2,7 +2,7 @@
 const postRoute=require("express").Router();
 
 postRoute.get("/",(req,res)=>{//"/"→useで使用しているパスの後ろに続けるエンドポイント
-    res.send("postrouter");
+    res.send("postrouter")
     
 })
 module.exports=postRoute
