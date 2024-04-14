d71ba43fabff8bedc849d92ee8b248ef9e952ba4
atuhon <kinnjiroa@gmail.com>
2024-04-15 00:53:49 +0900

    14日分

--- React/real-sns-front/src/components/share/Share.jsx
+++ React/real-sns-front/src/components/share/Share.jsx
@@ -3,6 +3,8 @@ import ImageIcon from '@mui/icons-material/Image';
 import GifIcon from '@mui/icons-material/Gif';
 import FaceIcon from '@mui/icons-material/Face';
 import AnalyticsIcon from '@mui/icons-material/Analytics';
+import '../share/Share.css';
+
 
 
 
@@ -11,22 +13,23 @@ export default function Share() {
     <div className="share">
         <div className="shareWrapper">
             <div className="shareTop">
-                <img src='/assets/person/1.jpeg' alt="" />
+                <img src='/assets/person/1.jpeg' alt="" className='shareProfileImg' />
                 <input type="text" className='shareInput' placeholder='今何してるの'/>
             </div>
             <hr className='shareHr'/>
             <div className="shareButtons">
               <div className="shareOptions">
                 <div className="shareOption">
-                  <ImageIcon className='ImageIcon'/>
+                  {/* htmlColor='blue'⇒色を変更できる */}
+                  <ImageIcon className='ImageIcon' htmlColor='blue'/>
                   <span className='shareOptionText'>写真</span>
                 </div>
                 <div className="shareOption">
-                  < GifIcon className='ImageIcon'/>
+                  < GifIcon className='ImageIcon' htmlColor='hotpink'/>
                   <span className='shareOptionText'>GIF</span>
                 </div>
                 <div className="shareOption">
-                  <FaceIcon className='ImageIcon'/>
+                  <FaceIcon className='ImageIcon' htmlColor='green'/>
                   <span className='shareOptionText'>気持ち</span>
                 </div>
                 <div className="shareOption">
@@ -34,10 +37,7 @@ export default function Share() {
                   <span className='shareOptionText'>投票</span>
                 </div>
               </div>
-            </div>
-
-            <div className="shareButtons">
-
+              <button className="shareButton">投稿</button>
             </div>
         </div>
     </div>
