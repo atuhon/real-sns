c291206018538d98aaacc9e8dc8faa1e2167c415
atuhon <kinnjiroa@gmail.com>
2024-04-15 23:32:47 +0900

    15日

--- React/real-sns-front/src/components/post/Post.jsx
+++ React/real-sns-front/src/components/post/Post.jsx
@@ -1,24 +1,39 @@
-import React from 'react'
-import '../post/post.css';
-import MoreVert from '@mui/icons-material/MoreVert';
-
+import React from "react";
+import "../post/post.css";
+import MoreVert from "@mui/icons-material/MoreVert";
 
 export default function Post() {
   return (
     <div className="post">
       <div className="postWarapper">
         <div className="postTop">
-          <img src="/assets/person/1.jpeg" alt="" className='postProfileImg' />
-          <span className='postUserName'>Shin Code</span>
-          <span className='postDate'>5分前</span>
+          <div className="postTopLeft">
+            <img
+              src="/assets/person/1.jpeg"
+              alt=""
+              className="postProfileImg"
+            />
+            <span className="postUserName">Shin Code</span>
+            <span className="postDate">5分前</span>
+          </div>
+          <div className="postright">
+            <MoreVert />
+          </div>
         </div>
-        <div className="postright">
-          <span>
-            <MoreVert/>
-          </span>
-
+        <div className="postCenter">
+          <span className="postText">SNSを自作中です</span>
+          <img src="/assets/post/1.jpeg" alt="" className="postImg" />
+          <div className="postBottom">
+          <div className="postBottomLeft">
+            <img src="./assets/heart.png" className="likeIcon" />
+            <span className="postLoleCounter">5人がいいねを押しました</span>
+          </div>
+          <div className="postBottomright">
+            <span className="postCommentText">4:コメント</span>
+            </div>
+          </div>
         </div>
       </div>
     </div>
-  )
+  );
 }
